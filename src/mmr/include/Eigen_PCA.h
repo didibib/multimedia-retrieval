c649c9926dfd64802f2a9c597c9ee094bd6e1405
D1oStar <zettosyuu@gmail.com>
2022-10-01 14:01:13 +0200

    Update Eigen_PCA.h

--- src/mmr/include/Eigen_PCA.h
+++ src/mmr/include/Eigen_PCA.h
@@ -1,28 +1,17 @@
 #include "pmp/visualization/SurfaceMeshGL.h"
 
-#include<iostream>
 #include<Eigen/Dense>
 #include<algorithm>
 
-using namespace std;
 using namespace Eigen;
-using namespace pmp;
 
-void PCA_calculate(SurfaceMeshGL _mesh) {
-    Matrix3Xf input;
-    Matrix3f cov;
-    MatrixXf temp;
-    
+void PCA_calculate(pmp::SurfaceMeshGL _mesh)
+{
+    MatrixXf input;
     for (auto v : _mesh.vertices())
-    {
-        temp(1) = _mesh.position(v)[1];
-        temp(2) = _mesh.position(v)[2];
-        temp(3) = _mesh.position(v)[3];
-        input.col(input.cols() + 1) = temp;
-    }
-        
-    Matrix3Xf centered = input.colwise() - input.colwise().mean();
-    cov = centered * centered.adjoint();
+        input.col(input.cols() + 1) = VectorXf(_mesh.position(v));
+    MatrixXf centered = input.colwise() - input.colwise().mean();
+    MatrixXf cov = centered * centered.adjoint();
     cov = cov.array() / 2.0f;
-    SelfAdjointEigenSolver<Matrix3f> eig(cov);
+    SelfAdjointEigenSolver<MatrixXf> eig(cov);
 }
