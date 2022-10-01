4d16eed9ada49f90dcfed5e7592e7282ecfd39a7
D1oStar <zettosyuu@gmail.com>
2022-10-01 16:21:26 +0200

    Update Eigen_PCA.h

--- src/mmr/include/Eigen_PCA.h
+++ src/mmr/include/Eigen_PCA.h
@@ -5,16 +5,19 @@
 
 using namespace Eigen;
 
-void PCA_calculate(pmp::SurfaceMeshGL _mesh)
+void pca_calculate(pmp::SurfaceMeshGL _mesh)
 {
     unsigned int n_vertices = _mesh.n_vertices();
-    MatrixXf in(n_vertices, 3);
+    MatrixXf input(3, n_vertices);
     unsigned int i = 0;
     for (auto v : _mesh.vertices())
-        in.row(i++) = RowVectorXf(_mesh.position(v));
-    MatrixXf input = in.transpose();
-    RowVectorXf mean = input.colwise().mean();
-    MatrixXf centered = input.rowwise() - mean;
+    {
+        input.row(i)[0] = _mesh.position(v)[0];
+        input.row(i)[1] = _mesh.position(v)[1];
+        input.row(i++)[2] = _mesh.position(v)[2];
+    }
+    VectorXf mean = input.rowwise().mean();
+    MatrixXf centered = input.colwise() - mean;
     MatrixXf cov = centered * centered.adjoint();
     cov = cov.array() / (input.rows() - 1);
     SelfAdjointEigenSolver<MatrixXf> eig(cov);
