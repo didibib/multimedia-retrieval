c172b22bff56a6424d78648249a1715102c41da1
unknown <tariquake@hotmail.com>
2022-10-15 19:30:27 +0200

    Simpler version of A3

--- src/mmr/src/descriptors.cpp
+++ src/mmr/src/descriptors.cpp
@@ -98,11 +98,10 @@ pmp::Scalar Descriptor::diameter(pmp::SurfaceMesh& mesh)
 
 Histogram Descriptor::D1(pmp::SurfaceMesh& mesh)
 {
-    size_t mesh_size(mesh.n_vertices());
     std::mt19937::result_type seed =
         std::chrono::high_resolution_clock::now().time_since_epoch().count();
     auto random =
-        std::bind(std::uniform_int_distribution<int>(0, mesh_size - 1),
+        std::bind(std::uniform_int_distribution<int>(0, mesh.n_vertices() - 1),
                   std::mt19937(seed));
     auto points = mesh.get_vertex_property<pmp::Point>("v:point");
     auto center = centroid(mesh);
@@ -118,11 +117,10 @@ Histogram Descriptor::D1(pmp::SurfaceMesh& mesh)
 
 Histogram Descriptor::D2(pmp::SurfaceMesh& mesh)
 {
-    size_t mesh_size(mesh.n_vertices());
     std::mt19937::result_type seed =
         std::chrono::high_resolution_clock::now().time_since_epoch().count();
     auto random =
-        std::bind(std::uniform_int_distribution<int>(0, mesh_size - 1),
+        std::bind(std::uniform_int_distribution<int>(0, mesh.n_vertices() - 1),
                   std::mt19937(seed));
 
     auto points = mesh.get_vertex_property<pmp::Point>("v:point");
@@ -162,48 +160,40 @@ Histogram Descriptor::A3(pmp::SurfaceMesh& mesh)
 
     auto points = mesh.get_vertex_property<pmp::Point>("v:point");
 
-    int k = pow(param::TARGET_VALUE, 1.0 / 3.0);
-
     // Use pointers to create create data on heap
     std::vector<float>* angles = new std::vector<float>();
     angles->reserve(param::TARGET_VALUE);
     int max_value = 0;
 
-    for (unsigned int i = 0; i < k; i++)
+    for (unsigned int i = 0; i < param::TARGET_VALUE; i++)
     {
         int v1 = random();
 
-        for (unsigned int j = 0; j < k; j++)
-        {
-            int v2 = random();
-            while (v2 == v1)
-                v2 = random();
+        int v2 = random();
+        while (v2 == v1)
+            v2 = random();
 
-            for (unsigned int l = 0; l < k; l++)
-            {
-                int v3 = random();
-                while (v3 == v2 || v3 == v1)
-                    v3 = random();
+        int v3 = random();
+        while (v3 == v2 || v3 == v1)
+            v3 = random();
 
-                pmp::Point p1 = points[pmp::Vertex(v1)];
-                pmp::Point p2 = points[pmp::Vertex(v2)];
-                pmp::Point p3 = points[pmp::Vertex(v3)];
+        pmp::Point p1 = points[pmp::Vertex(v1)];
+        pmp::Point p2 = points[pmp::Vertex(v2)];
+        pmp::Point p3 = points[pmp::Vertex(v3)];
 
-                auto u = p2 - p1;
-                auto v = p3 - p1;
+        auto u = p2 - p1;
+        auto v = p3 - p1;
 
-                float dot = pmp::dot(u, v);
-                float u_mag = pmp::distance(p2, p1);
-                float v_mag = pmp::distance(p3, p1);
+        float dot = pmp::dot(u, v);
+        float u_mag = pmp::distance(p2, p1);
+        float v_mag = pmp::distance(p3, p1);
 
-                float angle = acos(dot / (u_mag * v_mag));
+        float angle = acos(dot / (u_mag * v_mag));
 
-                if (angle > max_value)
-                    max_value = angle;
+        if (angle > max_value)
+            max_value = angle;
 
-                angles->push_back(angle);
-            }
-        }
+        angles->push_back(angle);
     }
 
     return Histogram(*angles, 0, max_value, param::BIN_SIZE);
