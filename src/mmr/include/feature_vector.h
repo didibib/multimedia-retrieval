ac5e16754a1f8a201fa97aabe06fee7662c1ee21
D1oStar <zettosyuu@gmail.com>
2022-10-26 00:50:55 +0200

    distance fixed

--- src/mmr/include/feature_vector.h
+++ src/mmr/include/feature_vector.h
@@ -6,8 +6,6 @@
 #include "emd.h"
 #include "descriptors.h"
 
-typedef float feature_t;
-
 float e_dist(feature_t *F1, feature_t *F2);
 Scalar dist(mmr::Histogram h1, mmr::Histogram h2);
 Scalar dist(std::map<std::string, Scalar> data1,
--- src/mmr/src/feature_vector.cpp
+++ src/mmr/src/feature_vector.cpp
@@ -4,17 +4,30 @@ using namespace pmp;
 
 Scalar e_dist(feature_t *F1, feature_t *F2)
 {
-    return *F1 - *F2;
+    return fabs(*F1 - *F2);
 }
 
 Scalar dist(mmr::Histogram *h1, mmr::Histogram *h2)
 {
-    Scalar w1[10], w2[10];
-    std::copy(h1->histogram.begin(), h1->histogram.end(), w1);
-    std::copy(h2->histogram.begin(), h2->histogram.end(), w2);
-    feature_t f1[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-              f2[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    signature_t s1 = {10, f1, w1}, s2 = {10, f2, w2};
+    std::vector<float>::size_type N1 = h1->histogram.size();
+    std::vector<float>::size_type N2 = h2->histogram.size();
+    feature_t *f1 = new feature_t[N1], *f2 = new feature_t[N2];
+    Scalar *w1 = new Scalar[N1], *w2 = new Scalar[N2];
+    int i = 0;
+    for (auto it:h1->histogram)
+    {
+        f1[i] = i;
+        w1[i] = it;
+        ++i;
+    }
+    i = 0;
+    for (auto it:h2->histogram)
+    {
+        f2[i] = i;
+        w2[i] = it;
+        ++i;
+    }
+    signature_t s1 = {N1, f1, w1}, s2 = {N2, f2, w2};
     Scalar distance = emd(&s1, &s2, e_dist, 0, 0);
     return distance;
 }
